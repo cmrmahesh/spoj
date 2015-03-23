7e98deae8afc5b633cfd7fcd437ad492b7121743
cmrmahesh <cmr.mahesh@gmail.com>
2015-03-23 23:25:55 +0530

    optimized fctrl

--- FCTRL2.py
+++ FCTRL2.py
@@ -1,10 +1,6 @@
+from math import factorial
 for _ in xrange(int(raw_input())):
-	i = int(raw_input()) 
-	fact = 1
 
-	for x in xrange(1,i+1):
-		fact *= x
+	print factorial(int(raw_input()))
 
-	print fact
 
-	
